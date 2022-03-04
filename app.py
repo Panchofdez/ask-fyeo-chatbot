2b95b607b091a17bdc24f38500a89228b64c8b6e
Pancho Fernandez <panchofern@gmail.com>
2022-03-04 14:35:49 -0500

    initial integration to use postgres

--- app.py
+++ app.py
@@ -3,10 +3,37 @@ import json
 import torch
 from model import NeuralNet
 from chat import chatbot
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+
 app = Flask(__name__)
+CORS(app)
+
+PRODUCTION = False
 
 
-#SETUP
+
+if PRODUCTION:
+    #database string needs to start with postgresql:// not postgres:// which is what heroku sets it to by default and is unchangeable
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    app.debug = False
+else:
+    load_dotenv() #load environment variables for local environment
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:SpicyP#13@localhost/ask-fyeo-chatbot'
+    app.debug = True
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('MY_SECRET_KEY')
+
+
+
+db = SQLAlchemy(app)
+
+from models import Conversation, Query, Staff
+#Model SETUP
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -31,20 +58,82 @@ context = {}
 
 
 
+
+
 @app.route('/')
 def hello():
     return 'Hello World'
 
+@app.route('/chat/start', methods=['POST'])
+def start():
+    if request.method == 'POST':
+        try:
+            firstname = request.json['firstname']
+            lastname = request.json['lastname']
+            program = request.json['program']
+            email = request.json['email']
 
+            convo = Conversation(firstname=firstname, lastname=lastname, program=program, email=email)
+            db.session.add(convo)
+            db.session.commit() 
+            return jsonify({'conversation': convo})
+        except Exception as e:
+            print("Error: ", e)
+            return jsonify({'error':'Error in starting conversation'}), 400
 
-@app.route('/answer', methods=['POST'])
-def predict():
+    return jsonify({'error':'Invalid request type'}), 400
 
+@app.route('/chat/<conversation_id>/answer', methods=['POST'])
+def predict(conversation_id):
     if request.method == 'POST':
         question = request.json['question']
         print(question)
         response = chatbot(question, data, model, intents, context=context)
-
+        conversation = Conversation.query.get(conversation_id)
+        query = Query(question=question, response=response, conversation_id=conversation.id)
+        db.session.add(query)
+        db.session.commit()
         print('RESPONSE ', response)
-        return jsonify({'response': response})
-    return jsonify({'error':'Invalid request type'})
+        return jsonify({ 'query': query })
+    return jsonify({'error':'Invalid request type'}), 400
+
+
+
+@app.route('/chat/<conversation_id>/resolve/<query_id>', methods=['PUT'])
+def resolve(conversation_id, query_id):
+    if request.method == 'PUT':
+        try:
+            conversation = Conversation.query.get(conversation_id)
+            query = Query.query.get(query_id)
+            if query.conversation_id != conversation.id:
+                return jsonify({'error':'Wrong conversation details'}), 400
+
+            query.resolved = True
+            db.session.commit()
+
+            return jsonify({'query': query })
+        except Exception as e:
+            print("Error: ", e)
+            return jsonify({'error':'Error in starting conversation'}), 400
+
+    return jsonify({'error':'Invalid request type'}), 400
+
+@app.route('/chat/<conversation_id>/contact', methods=['PUT'])
+def contact(conversation_id):
+    if request.method == 'PUT':
+        try:
+
+            conversation = Conversation.query.get(conversation_id)
+            conversation.contact = True
+
+            db.session.commit()
+
+            return jsonify({'conversation': conversation})
+        except Exception as e:
+            print("Error: ", e)
+            return jsonify({'error':'Error in starting conversation'}), 400
+
+    return jsonify({'error':'Invalid request type'}), 400
+
+if __name__ == "__main__":
+    app.run()
---
+++ models.py
@@ -0,0 +1,55 @@
+from app import db
+from datetime import datetime
+from dataclasses import dataclass
+
+
+
+@dataclass
+class Conversation(db.Model):
+    __tablename__ = 'conversation'
+    id:int
+    firstname: str
+    lastname: str
+    program:str
+    email: str
+    contact:bool
+    date: datetime
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(200), nullable=False)
+    lastname = db.Column(db.String(200), nullable=False)
+    program = db.Column(db.String(200))
+    email = db.Column(db.String(200), nullable=False)
+    contact = db.Column(db.Boolean, nullable=False,  default=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    queries = db.relationship('Query', backref='author', lazy=True)
+
+
+
+@dataclass
+class Query(db.Model):
+    __tablename__ = 'query'
+    id:int
+    question: str
+    response: str
+    resolved: bool
+    conversation_id: int
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(300), nullable=False)
+    response = db.Column(db.String(300), nullable=False)
+    resolved = db.Column(db.Boolean, nullable=False, default=False)
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'), nullable=False)
+
+
+
+@dataclass
+class Staff(db.Model):
+    id:int
+    email:str
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    

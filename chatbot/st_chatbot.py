ed470fe4a801d3514f50a64a3e1c522958c426fe
Panchofdez <panchofern@gmail.com>
2024-10-22 10:24:21 -0400

    cleaned up print statements

--- chatbot/st_chatbot.py
+++ chatbot/st_chatbot.py
@@ -18,7 +18,6 @@ class STChatbot(Chatbot):
     def get_response(self, query, data, file=None): 
 
         default_answer = ("", "Hmm... I do not understand that question. Please try again or ask a different question")
-        # final_scores = []
         sentence_tag_map = {}
         all_sentences = []
         
@@ -30,11 +29,7 @@ class STChatbot(Chatbot):
                 clean_sent = remove_punc(sent.lower())
                 sentence_tag_map[clean_sent] = tag
                 all_sentences.append(clean_sent)
-                
-                
-        # print("SENTENCES", all_sentences)
-                
-                
+                   
         ranks = self.sentence_transformer_model.rank(remove_punc(query.lower()), all_sentences)
 
         high_score = None
@@ -48,19 +43,7 @@ class STChatbot(Chatbot):
 
         print("MAX_SCORE", high_score, target_tag)
         score_result = (target_tag, high_score)
-            # print("HIGH_SCORE", high_score)
-            # final_scores.append((tag, high_score, random.choice(intent["responses"])))
-        
-        # print("FINAL SCORES", final_scores)
-        # max_score = None
-        # high_scores = []
-        # for tag, score, resp in final_scores:
-        #     if (max_score is None or score > max_score) and score > 0.45:
-        #         result = (tag, resp)
-        #         max_score = score
-        #         high_scores.append((tag, score))
 
-        # print("MAX_SCORE", max_score)    
         print("HIGH SCORES", high_scores[-5:])    
         print("FINAL RESULT", score_result)        
         for intent in data["intents"]:
@@ -90,17 +73,11 @@ class STChatbot(Chatbot):
        
         question = tokenize(question)
         patterns = ' '.join(patterns)
-        #print("Question", question)
-        #print('Patterns',patterns)
-        # print("Response", response)
         ignore_words = ['?', '!', '.', ',', 'are', 'you', 'can', 'and', 'let','where', 'why', 'what', 'how' , 'when', 'who', 'the' , 'need', 'for', 'have', 'but']
         stemmed_words  = [stem(w) for w in question if w.lower() not in ignore_words and len(w) > 2 ] # avoid punctuation or words like I , a , or 
-        
 
         if len(stemmed_words) == 0:
             stemmed_words = [stem(w) for w in question]
-
-        #print(stemmed_words)
 
         found = [ w for w in stemmed_words if re.search(w.lower(), response.lower()) or re.search(w.lower(),tag.lower() ) != None or re.search(w.lower(), patterns.lower())] #check if the question has words related in the response
         print("FOUND", found)

1f8efd587b87f7728d0aa0dee411568086a5057d
Panchofdez <panchofern@gmail.com>
2025-02-10 13:55:03 -0500

    removed nltk downloads

--- chatbot/nltk_utils.py
+++ chatbot/nltk_utils.py
@@ -1,7 +1,7 @@
 import nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+# nltk.download('wordnet')
 import numpy as np
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
